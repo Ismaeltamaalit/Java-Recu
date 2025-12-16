5356f80cf822d9be72f572c8412112f33cbb6ad0
Ismaeltamaalit <ismatamaalit@gmail.com>
2025-12-16 15:21:34 +0100

    ertetet

--- src/dinamicoLineal1/ListaArray.java
+++ src/dinamicoLineal1/ListaArray.java
@@ -4,13 +4,13 @@ public class ListaArray {
     private Object[] elementos;
     private int contador;
 
-    // Constructor: empezamos con un espacio pequeño (ej. 2)
+    
     public ListaArray() {
         elementos = new Object[2];
         contador = 0;
     }
 
-    // Añadir un elemento al final
+   
     public void añadir(Object obj) {
         if (contador == elementos.length) {
             redimensionar();
@@ -19,7 +19,7 @@ public class ListaArray {
         contador++;
     }
 
-    // Obtener un elemento por su posición
+    
     public Object obtener(int indice) {
         if (indice < 0 || indice >= contador) {
             return "Error: Índice fuera de rango";
@@ -27,7 +27,7 @@ public class ListaArray {
         return elementos[indice];
     }
 
-    // Doblar el tamaño del array cuando se llena
+    
     private void redimensionar() {
         Object[] nuevoArray = new Object[elementos.length * 2];
         for (int i = 0; i < elementos.length; i++) {
@@ -37,7 +37,7 @@ public class ListaArray {
         System.out.println("...Lista llena. Aumentando espacio a: " + elementos.length);
     }
 
-    // Cuántos elementos hay realmente
+    
     public int tamaño() {
         return contador;
     }
